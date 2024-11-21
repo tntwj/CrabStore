28e2876e135c32efdace941baf50806889298f13
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-11-21 15:55:27 +0100

    Removed references to username

--- php/notifications.php
+++ php/notifications.php
@@ -3,7 +3,7 @@ require_once("bootstrap.php");
 
 $templateParams["title"] = "Crabstore - Notifications";
 $templateParams["main-content"] = "template/customer-notifications.php";
-$templateParams["notifications"] = $dbh->getUserNotifications("jane_smith");
+$templateParams["notifications"] = $dbh->getUserNotifications("jane.smith@example.com");
 
 require_once("template/base.php");
 ?>
