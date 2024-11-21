a5f97aa489284e983085d651f31b54f7851bda24
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-21 11:31:50 +0100

    fixed a bug

--- php/index.php
+++ php/index.php
@@ -2,6 +2,7 @@
 require_once("bootstrap.php");
 
 $templateParams["title"] = "Crabstore";
+$templateParams["nome"] = "cart-of-customer.php";
 
 require_once("template/base.php");
 ?>
--- php/template/cart-of-customer.php
+++ php/template/cart-of-customer.php
@@ -1,12 +1,12 @@
-<h2>Carrello</h2>
+<h2>Cart</h2>
 <ul>
 <?php if(isset($templateParams["products-cart"])): ?>
 <?php foreach($templateParams["products-cart"] as $product): ?>
     <li>
-        <img src="<?php echo UPLOAD_DIR.$product["image"][0]; ?>" alt="<?php $product["name"]; ?>"/>
+        <img src="<?php echo UPLOAD_DIR.$product["image"][0]; ?>" alt="<?php echo $product["name"]; ?>"/>
         <h3><?php echo $product["name"]; ?></h3>
         <input type="number" id="quantity" name="quantity" min="1" max="6" step="1" value="<?php echo $product["amount"]; ?>">
-        <p><?php echo "$".$product["finalPrice"] * 2; ?></p>
+        <p><?php echo "$".$product["finalPrice"] * $product["amount"]; ?></p>
     </li>
 </ul>
 <?php endforeach ?>
