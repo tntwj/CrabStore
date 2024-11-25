5badaa3403a553a6a3e96349c8eb68bd557534ae
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-11-25 11:22:33 +0100

    Added redirect

--- php/notifications.php
+++ php/notifications.php
@@ -1,6 +1,15 @@
 <?php
 require_once("bootstrap.php");
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isUserLoggedIn()) {
+    header("Location: login.php");
+    exit();
+}
+
 $templateParams["title"] = "Crabstore - Notifications";
 $templateParams["main-content"] = "template/customer-notifications.php";
 $templateParams["notifications"] = $dbh->getUserNotifications("jane.smith@example.com");
