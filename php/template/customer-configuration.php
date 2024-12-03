9ba1bb8b35186906541da6a676f781e3f5906856
Weijie Fu <weijie.fu@studio.unibo.it>
2024-12-03 21:26:22 +0100

    optimized with bootstrap

--- php/template/customer-configuration.php
+++ php/template/customer-configuration.php
@@ -1,23 +1,41 @@
-<h3>Buy <?php echo $templateParams['single-product-details']['name']?></h3>
-<img src="./upload/categories/<?php echo $templateParams['media']?>" alt="<?php echo $templateParams['single-product-details']['name']?>"/>
+<div class="container py-5">
+    <h3 class="text-center mb-4">Buy <?php echo $templateParams['single-product-details']['name']; ?></h3>
 
-<h4>Which is best for you?</h4>
-<ul class="list-group list-group-flush">
-<?php 
-$data = json_decode($templateParams['single-product-details']['specSheet'], true);
-foreach ($data as $key=>$info): 
-?>
-<li class="list-group-item">
-    <?php 
-    if (is_array($info)) {
-        echo $key.": ";
-        foreach ($info as $value) {
-            echo '| '.$value.' ';
-        }                                
-    }
-    ?>
-</li>
-<?php endforeach ?> 
-</ul>
+    <div class="text-center mb-4">
+        <img src="./upload/categories/<?php echo $templateParams['media']; ?>" 
+             alt="<?php echo $templateParams['single-product-details']['name']; ?>" 
+             class="img-fluid rounded" style="max-width: 300px;">
+    </div>
 
-<input type="submit">Add to cart</input>
+    <form method="POST" action="add_to_cart.php">
+        <?php 
+        $data = json_decode($templateParams['single-product-details']['specSheet'], true);
+        foreach ($data as $key => $info): ?>
+            <?php if (is_array($info)): // Only process if $info is an array ?>
+                <div class="mb-4">
+                    <!-- Key Header -->
+                    <h5 class="mb-3"><?php echo $key; ?> <span class="text-muted">(Select one)</span></h5>
+                    
+                    <!-- Option Cards -->
+                    <div class="row row-cols-1 row-cols-md-3 g-3">
+                        <?php foreach ($info as $index => $option): ?>
+                            <div class="col">
+                                <div class="card h-100 border-0 shadow-sm">
+                                    <div class="card-body text-center">
+                                        <p class="card-text mb-0"><?php echo $option; ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
+        
+
+        <div class="text-center mt-4">
+            <button type="submit" class="btn btn-primary btn-lg">Add to Cart</button>
+        </div>
+    </form>
+</div>
+
