c9df59fa7b40498bb2f09f26881b18dad1bed68d
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-12-07 00:22:06 +0100

    Reworked account details module

--- php/template/account-details.php
+++ php/template/account-details.php
@@ -2,27 +2,17 @@
     $accountDetails = $templateParams["account-details"][0];
     $totalOrders = $templateParams["total-orders"];
 ?>
-
-<div class="container mt-4">
-    <div class="row">
-        <div class="col-12 col-md-6">
-            <div class="card mb-3">
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-lg-6">
+            <div class="card shadow border">
                 <div class="card-body">
-                    <h5 class="card-title">Account Details</h5>
-                    <p><strong>First Name:</strong> <?php echo $accountDetails["firstName"]; ?></p>
-                    <p><strong>Last Name:</strong> <?php echo $accountDetails["lastName"]; ?></p>
-                    <p><strong>Email:</strong> <?php echo $accountDetails["email"]; ?></p>
-                    <p><strong>Joined On:</strong> <?php echo $accountDetails["joinDate"]; ?></p>
-                    <button class="btn btn-primary">Edit Profile</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-md-6">
-            <div class="card mb-3">
-                <div class="card-body">
-                    <h5 class="card-title">Additional Information</h5>
-                    <p><strong>Total Orders Made:</strong> <?php echo $totalOrders; ?></p>
+                    <h1 class="card-title text-dark text-center mb-4">Account Details</h1>
+                    <p><strong>First Name: </strong><?php echo $accountDetails["firstName"]; ?></p>
+                    <p><strong>Last Name: </strong><?php echo $accountDetails["lastName"]; ?></p>
+                    <p><strong>Email: </strong><?php echo $accountDetails["email"]; ?></p>
+                    <p><strong>Joined On: </strong><?php echo $accountDetails["joinDate"]; ?></p>
+                    <p><strong>Total Orders Made: </strong><?php echo $totalOrders; ?></p>
                 </div>
             </div>
         </div>
