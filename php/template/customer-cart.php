a7ea5e7d2f6ff1eb51b4ff4c4642c250e612617f
Weijie Fu <weijie.fu@studio.unibo.it>
2024-12-10 17:30:04 +0100

    use reference instead of value in products-cart loop

--- php/template/customer-cart.php
+++ php/template/customer-cart.php
@@ -6,37 +6,35 @@
     <ul class="list-group">
         <?php
         $totalPrice = 0;
-        $customPrice = 0;
         ?>
-        <?php foreach($templateParams["products-cart"] as $product): ?>
-        <li class="list-group-item d-flex align-items-center justify-content-between">
-            <div class="d-flex align-items-center">
-                <img src="<?php echo UPLOAD_DIR."products/".$product["image"][0]["imageUrl"]; ?>" 
-                        alt="<?php echo $product["name"]; ?>" 
-                        class="img-thumbnail me-3" 
-                        style="width: 100px; height: auto;">
-                <div>
-                    <h5 class="mb-1"><?php 
-                    echo $product["name"]; 
-                    foreach($product["options"] as $option) {
-                        echo " ".$option["details"];
-                        $customPrice += $option["price"];
-                    }
-                    ?></h5>
-                    <input type="number" 
-                            id="quantity" 
-                            name="quantity" 
-                            min="1" max="6" 
-                            step="1" 
-                            value="<?php echo $product["amount"]; ?>" 
-                            class="form-control form-control-sm w-50">
+        <?php foreach($templateParams["products-cart"] as &$product): ?>
+            <li class="list-group-item d-flex align-items-center justify-content-between">
+                <div class="d-flex align-items-center">
+                    <img src="<?php echo UPLOAD_DIR."products/".$product["image"][0]["imageUrl"]; ?>" 
+                            alt="<?php echo $product["name"]; ?>" 
+                            class="img-thumbnail me-3" 
+                            style="width: 100px; height: auto;">
+                    <div>
+                        <h5 class="mb-1"><?php 
+                        echo $product["name"]; 
+                        foreach($product["options"] as $option) {
+                            echo " ".$option["details"];
+                        }
+                        ?></h5>
+                        <input type="number" 
+                                id="quantity" 
+                                name="quantity" 
+                                min="1" max="6" 
+                                step="1" 
+                                value="<?php echo $product["amount"]; ?>" 
+                                class="form-control form-control-sm w-50">
+                    </div>
                 </div>
-            </div>
-            <p class="mb-0"><strong><?php echo "$".$product["finalPrice"] * $product["amount"]; ?></strong></p>
-        </li>
-        <?php 
-        $totalPrice += (($product["finalPrice"] * $product["amount"]) + $customPrice); 
-        ?>
+                <p class="mb-0"><strong><?php echo "$".$product["finalPrice"] * $product["amount"]; ?></strong></p>
+            </li>
+            <?php 
+            $totalPrice += ($product["finalPrice"] * $product["amount"]); 
+            ?>
         <?php endforeach ?>
     </ul>
     <p class="text-end mt-3"><strong>Your bag total is: $<?php echo $totalPrice; ?></strong></p>
